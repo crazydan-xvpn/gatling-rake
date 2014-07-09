a6724d0a3f62e8ffdd43483714e65f60d72591b8
iainjmitchell <iainjmitchell@gmail.com>
2014-07-09 16:28:28 +0100

    run gatling from correct location

--- tests/RunGatlingTests.rb
+++ tests/RunGatlingTests.rb
@@ -14,6 +14,17 @@ class RunGatlingTests < Test::Unit::TestCase
 		commands[0].should eql("rmdir /s /q #{result_directory}")
 	end
 
+	def test_that_gatling_is_run_from_correct_location
+		@commands = []
+		gatling_file_location = "aPlace/#{rand(4)}/gatling.bat"
+		mockShell = self
+		Gatling.new(mockShell).start(gatling_file_location: gatling_file_location)
+		gatling_command = commands[1].split(' ')
+		gatling_command[0].should eql(gatling_file_location)
+	end
+
+
+# %GATLING_DIR%\\gatling.bat -bf load-tests\request-bodies -sf load-tests\simulations -df load-tests\data -rf %teamcity.build.checkoutDir%\load-testing-results -s FlavaIt.FlavaItSimulation -sd "Flava-It load tests"
 	def execute(command)
 		@commands.push(command)
 	end
@@ -21,20 +32,31 @@ end
 
 class Gatling 
 	def initialize(shell)
+		@shell = shell
 		@remove_directory = RemoveDirectoryCommand.new(shell)
+		@run_gatling = GatlingCommand.new(shell)
 	end
 
 	def start(parameters)
 		@remove_directory.execute(parameters[:result_directory])
+		@run_gatling.execute(parameters[:gatling_file_location])
 	end
 end
 
-class RemoveDirectoryCommand
+class ShellCommand
 	def initialize(shell)
 		@shell = shell
 	end
+end
 
+class RemoveDirectoryCommand < ShellCommand
 	def execute(directory_name)
 		@shell.execute("rmdir /s /q #{directory_name}")
+	end
+end
+
+class GatlingCommand < ShellCommand
+	def execute(gatling_file_location)
+		@shell.execute(gatling_file_location)
 	end
 end
