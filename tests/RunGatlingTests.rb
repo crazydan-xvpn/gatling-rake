670a43727a468721c367af41dcfa19ee1a7fefb1
iainjmitchell <iainjmitchell@gmail.com>
2014-07-09 17:26:33 +0100

    adding request bodies location

--- tests/RunGatlingTests.rb
+++ tests/RunGatlingTests.rb
@@ -19,10 +19,18 @@ class RunGatlingTests < Test::Unit::TestCase
 		gatling_file_location = "aPlace/#{rand(4)}/gatling.bat"
 		mockShell = self
 		Gatling.new(mockShell).start(gatling_file_location: gatling_file_location)
-		gatling_command = commands[1].split(' ')
-		gatling_command[0].should eql(gatling_file_location)
+		gatling_command = commands[1]
+		gatling_command.should match(/^#{gatling_file_location}/)
 	end
 
+	def test_that_gatling_is_run_with_request_bodies_folder_specified
+		@commands = []
+		load_test_root = "aPlace/#{rand(4)}/"
+		mockShell = self
+		Gatling.new(mockShell).start(load_test_root: load_test_root)
+		gatling_command = commands[1]
+		gatling_command.should match(/-bf #{load_test_root}\/request-bodies/)
+	end
 
 # %GATLING_DIR%\\gatling.bat -bf load-tests\request-bodies -sf load-tests\simulations -df load-tests\data -rf %teamcity.build.checkoutDir%\load-testing-results -s FlavaIt.FlavaItSimulation -sd "Flava-It load tests"
 	def execute(command)
@@ -39,7 +47,10 @@ class Gatling
 
 	def start(parameters)
 		@remove_directory.execute(parameters[:result_directory])
-		@run_gatling.execute(parameters[:gatling_file_location])
+		@run_gatling.execute(
+			gatling_file_location: parameters[:gatling_file_location],
+			load_test_root: parameters[:load_test_root]
+		)
 	end
 end
 
@@ -56,7 +67,17 @@ class RemoveDirectoryCommand < ShellCommand
 end
 
 class GatlingCommand < ShellCommand
-	def execute(gatling_file_location)
-		@shell.execute(gatling_file_location)
+	def execute(parameters)
+		load_test_root = parameters[:load_test_root]
+		gatling_parameters = 
+			{
+				'-bf' => "#{load_test_root}/request-bodies"
+			}
+			.map{| key , value | "#{key} #{value}"}
+			.join
+
+		
+		gatling_file_location = parameters[:gatling_file_location]
+		@shell.execute("#{gatling_file_location} #{gatling_parameters}")
 	end
 end
